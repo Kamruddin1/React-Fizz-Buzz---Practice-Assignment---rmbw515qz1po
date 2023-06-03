c0fe7e07211c1960aaf39b256273e46c79fa03c8
kamruddin Ansari <114280257+Kamruddin1@users.noreply.github.com>
2023-06-03 16:32:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+
+  const [count, setCount] = useState(1);
+ 
+
+  const handleIncrement = () =>{
+    setCount((prevState) => prevState + 1);
+   
+  }
+  const handleDecrement = () =>{
+    setCount((prevState) => prevState - 1);
+  }
+
+  const getColorClassName = () =>{
+    if(count % 3 == 0 && count % 5 == 0){
+      return "fizzbuzz";
+    }
+    else if(count % 3 == 0){
+      return "fizz";
+    }
+    else if(count % 5 == 0){
+      return "buzz";
+    }
+    else{
+      return "normal";
+    }
+  }
 
   
   return (
     <div id="main">
+
+      <button onClick={handleIncrement}>increment</button>
+      <p className={getColorClassName()}>{count}</p>
+      
+      <button onClick={handleDecrement}>decrement</button>
 
     </div>
   )
